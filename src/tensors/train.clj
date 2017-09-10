0e3a5a6b997316fffb7b2963adde9feb0dad9976
Aria Haghighi <me@aria42.com>
2017-09-10 12:52:19 -0700

    minor training refactor

--- src/tensors/train.clj
+++ src/tensors/train.clj
@@ -22,41 +22,51 @@
   {:num-iters 10
    :learning-rate 0.01})
 
+(defn reset-batch!
+  "Reset all gradients except the target node"
+  [loss-node]
+  ;; zero out gradients for parameters and ops nodes
+  (doseq [node (graph/post-order-nodes loss-node)
+          :when (#{:op :params} (:type node))]
+    (tensors/fill!
+     (p/safe-get node :factory)
+     (p/safe-get node :grad)
+     (fn ^double [] 0.0)))
+  ;; put a 1.0 on top-level gradient so backward pass
+  ;; can propogate non-zero grads backwards
+  (tensors/fill!
+   (p/safe-get loss-node :factory)
+   (p/safe-get loss-node :grad)
+   (fn ^double [] 1.0)))
+
+(defn update-params! [model opts]
+  ;; take gradient step
+  (doseq [[_ param-node] model]
+    (tensors/grad-step!
+     (p/safe-get param-node :factory)
+     (p/safe-get param-node :value)
+     (p/safe-get opts :learning-rate)
+     (p/safe-get param-node :grad))))
+
+(defn run-batch! [model loss-node batch]
+  (let [factory (p/safe-get loss-node :factory)]
+    (loop [batch-loss 0.0 batch batch]
+      (if-let [input->vals (first batch)]
+        (let [loss-node (compute/forward-pass! loss-node input->vals)
+              loss-val (->> loss-node :value (tensors/->clj factory) first)]
+          ;; side-effect to update gradients
+          (compute/backward-pass! loss-node)
+          (recur (+ batch-loss 0.0 (double loss-val)) (next batch)))
+        batch-loss))))
+
 (defn sgd-iter! [model loss-node data-gen opts]
-  (let [factory (p/safe-get loss-node :factory)
-        total-loss (atom 0.0)]
+  (let [total-loss (atom 0.0)]
     (doseq [batch (data-gen)]
-      ;; zero out gradients for everything
-      (doseq [node (graph/post-order-nodes loss-node)
-              :when (:grad node)]
-        (tensors/fill!
-         (p/safe-get node :factory)
-         (p/safe-get node :grad)
-         (fn ^double [] 0.0)))
-      ;; put a 1.0 on top-level gradient
-      (tensors/fill! factory (:grad loss-node) (fn ^double [] 1.0))
-      ;; run forward-backward on the batch
-      ;; gradient accumulates deltas
-      (let [batch-loss (atom 0.0)
-            batch-counter (atom 0)]
-        (doseq [input->vals batch]
-          (-> loss-node
-              (compute/forward-pass! input->vals)
-              compute/backward-pass!)
-#_          (println "batch: " (seq model))
-          (let [loss-val (->> loss-node :value (tensors/->clj factory) first)]
-            (swap! batch-loss + loss-val)))
-#_        (printf "batch-loss: %.3f\n" @batch-loss)
-        (swap! total-loss + @batch-loss))
-      ;; take gradient step
-      (doseq [[_ param-node] model]
-        (tensors/grad-step!
-         (p/safe-get param-node :factory)
-         (p/safe-get param-node :value)
-         (p/safe-get opts :learning-rate )
-         (p/safe-get param-node :grad)))
-#_      (println "mode: " (seq model)))
-    (let [grads (mapcat (fn [[_ x]] (flatten (tensors/->clj factory (:grad x)))) model)
+      (reset-batch! loss-node)
+      (let [batch-loss (run-batch! model loss-node batch)]
+        (swap! total-loss + batch-loss))
+      (update-params! model opts))
+    (let [grads (mapcat (fn [[_ x]] (flatten (tensors/->clj (:factory x) (:grad x)))) model)
           l2-norm (Math/sqrt (reduce (fn [res x] (+ res (* x x))) grads))]
       (printf "l2-norm: %.3f\n" l2-norm))
     @total-loss))
