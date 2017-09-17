2f6da209718f7982e89ed823caf41abaac365545
Aria Haghighi <me@aria42.com>
2017-09-17 14:22:44 -0700

    Attempt at batching operations

---
+++ test/tensors/batch_bench_test.clj
@@ -0,0 +1,40 @@
+(ns tensors.batch-bench-test
+  (:use [uncomplicate.neanderthal core native]
+        [criterium core]
+        [tensors neanderthal-ops])
+  (:require [tensors.graph :as graph]
+            [tensors.core :as tensors]
+            [tensors.compute :as compute]
+            [uncomplicate.neanderthal.core :as np]
+            [uncomplicate.neanderthal.native :as native]
+            [uncomplicate.neanderthal.real :as real]
+            [schema.core :as s]
+            [plumbing.core :as p]))
+
+(defn gen-data [n]
+  (let [num-classes 10
+        num-feats 500
+        M (dge num-classes num-feats)
+        r (java.util.Random. 0)
+        vecs (mapv (fn [_] (dge num-feats 1)) (range n))]
+    (alter! M (fn ^double [^double x] (.nextDouble ^java.util.Random r)))
+    (doseq [v vecs]
+      (alter! v (fn ^double [^double x] (if (.nextBoolean r) 1.0 0.0))))
+    (let [param {:ref-name "M" :shape [num-classes num-feats] :value M}]
+      (mapv (fn [v]
+              {:shape [num-classes 1]
+               :value (dge num-classes 1)
+               :children [param {:shape [num-feats 1] :value v}]})
+            vecs))))
+
+(defn serial-version [nodes]
+  (let [op (->MultTensorOp)]
+    (doseq [n nodes]
+      (compute/forward-node-pass! op n))
+    nodes))
+
+(defn batch-version [nodes]
+  (let [op (->MultTensorOp)
+        sig (compute/batch-signature op (first nodes))]
+    (compute/batch-forward-node-pass! op sig nodes)))
+
