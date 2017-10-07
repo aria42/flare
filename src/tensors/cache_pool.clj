77f7731e6083c707fefb717391e7d3bc717ef0c5
Aria Haghighi <me@aria42.com>
2017-10-07 10:43:23 -0700

    clean up debugging

--- src/tensors/cache_pool.clj
+++ src/tensors/cache_pool.clj
@@ -17,9 +17,7 @@
       (get-obj [this k]
         (let [^LinkedBlockingQueue q (.computeIfAbsent cmh k queue-fn)]
           (if-let [v (.poll q)]
-            (do
-              #_(println "Cache hit for " k " count now " (cache-pool/obj-count this k))
-              v)
+            v
             (get-fn k))))
       (obj-count [this k]
         (if-let [^LinkedBlockingQueue q (.get cmh k)]
@@ -28,5 +26,4 @@
       (return-obj [this k v]
         (let [^LinkedBlockingQueue q (.computeIfAbsent cmh k queue-fn)
               cnt (cache-pool/obj-count this k)]
-          (.offer q v)
-          #_(println "Return cache on key " k " count: " cnt " -> " (cache-pool/obj-count this k)))))))
+          (.offer q v))))))
