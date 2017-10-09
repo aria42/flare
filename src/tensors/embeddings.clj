32f14b57d228199ec1c6612b04afd5c5f2d3a49a
Aria Haghighi <me@aria42.com>
2017-10-09 15:22:30 -0400

    embedings and bilstm

---
+++ src/tensors/embeddings.clj
@@ -0,0 +1,39 @@
+(ns tensors.embeddings
+  (:require [schema.core :as s]
+            [tensors.computation-graph :as cg]
+            [tensors.core :as tensors]))
+
+(defprotocol Embedding
+  (lookup [this obj])
+  (vocab [this])
+  (embedding-size [this]))
+
+(deftype FixedEmbedding [^java.util.Map m ^long emb-size]
+  Embedding
+  (lookup [this obj] (.get m obj))
+  (vocab [this] (seq (.keySet m)))
+  (embedding-size [this] emb-size)
+
+  clojure.lang.Seqable
+  (seq [this] (map (juxt key val) m)))
+
+(defn fixed-embedding
+  [factory emb-size obj-vec-pairs]
+  (let [m (java.util.HashMap.)
+        expected-shape [emb-size]]
+    (doseq [[obj nums] obj-vec-pairs]
+      (when-let [dupe (.get m obj)]
+        (throw (ex-info "Duplicate entry" {:dupe obj})))
+      (let [t (tensors/from-nums factory nums)
+            s (tensors/shape factory t)]
+        (when (not= s expected-shape)
+          (throw (ex-info "embedding doesn't have same shape"
+                          {:expected [embedding-size] :actual s})))
+        (.put m obj t)))
+    (FixedEmbedding. m (long emb-size))))
+
+(defn read-text-embedding-pairs [rdr]
+  (for [^String line (line-seq rdr)
+        :let [fields (.split line " ")]]
+    [(aget fields 0)
+     (map #(Double/parseDouble ^String %) (rest fields))]))
---
+++ src/tensors/examples/bilstm_tag.clj
@@ -0,0 +1,59 @@
+(ns tensors.examples.bilstm-tag
+  (:require [tensors.node :as node]
+            [tensors.rnn :as rnn]
+            [tensors.embeddings :as embeddings]
+            [clojure.java.io :as io]
+            [clojure.tools.cli :refer [parse-opts]]
+            [tensors.neanderthal-ops :as no]
+            [tensors.model :as model]))
+
+(defn build-graph [words embed rnn-cell])
+
+(def cli-options
+  ;; An option with a required argument
+  [["-f" "--file PATH" "path to data"
+    :default "data/small-glove.50d.txt"]
+   ["-s" "--emb-size NUM" "size of embedding data"
+    :default 50]
+   ["-l" "--lstm-size NUM" "lstm size"]])
+
+(defn gen-sentence [n]
+  (take n (repeatedly #(first (shuffle #{"the" "dog" "barks"})))))
+
+(defn load-embeddings [opts]
+  (let [factory (no/->Factory)]
+    (embeddings/fixed-embedding
+     (no/->Factory)
+     (:emb-size opts)
+     (-> opts :file io/reader embeddings/read-text-embedding-pairs))))
+
+(comment
+  (def opts {:file "data/small-glove.50d.txt" :lstm-size 10 :emb-size 50})
+  (def factory (no/->Factory))
+  (def emb (load-embeddings opts))
+  (def model (model/simple-param-collection factory))
+  (def cell (rnn/lstm-cell model (:emb-size opts) (:lstm-size opts)))
+  (def sent-words (gen-sentence 10))
+  (def sent-nodes (mapv (fn [w]
+                          (node/constant
+                           [(:emb-size opts)]
+                           (embeddings/lookup emb w)))
+                        sent-words))
+  (def hiddens (rnn/build-seq cell sent-nodes)))
+
+(defn run [opts]
+  (let [factory (no/->Factory)
+        emb (load-embeddings opts)
+        model (model/simple-param-collection factory)
+        cell (rnn/lstm-cell model (:emb-size opts) (:lstm-size opts))
+        sent-words (gen-sentence 10)
+        sent-nodes (mapv (fn [w]
+                           (node/constant
+                            [(:emb-size opts)]
+                            (embeddings/lookup emb w)))
+                         sent-words)
+        hiddens (rnn/build-seq cell sent-nodes)]))
+
+(defn -main [& args]
+  (let [parse (parse-opts args cli-options)]
+    (run (:options parse))))
